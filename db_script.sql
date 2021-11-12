5153e8ac0229e48e4de0cbfc0b7c1dacae68ba0c
RobinMeykens-PXL <11800759@student.pxl.be>
2021-11-12 16:06:45 +0100

    script

--- db_script.sql
+++ db_script.sql
@@ -1,12 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 4.8.0.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Gegenereerd op: 29 feb 2020 om 17:22
--- Serverversie: 10.1.32-MariaDB
--- PHP-versie: 7.2.5
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
@@ -18,17 +9,8 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- Database: `employees`
---
 CREATE DATABASE IF NOT EXISTS `employees` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE `employees`;
-
--- --------------------------------------------------------
-
---
--- Tabelstructuur voor tabel `employees`
---
 
 DROP TABLE IF EXISTS `employees`;
 CREATE TABLE `employees` (
@@ -36,10 +18,6 @@ CREATE TABLE `employees` (
   `first_name` varchar(120) NOT NULL,
   `last_name` varchar(120) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Gegevens worden geëxporteerd voor tabel `employees`
---
 
 INSERT INTO `employees` (`emp_no`, `first_name`, `last_name`) VALUES
 (1001, 'Dries', 'Swinnen'),
@@ -49,13 +27,6 @@ INSERT INTO `employees` (`emp_no`, `first_name`, `last_name`) VALUES
 (1005, 'David', 'Parren'),
 (1006, 'Gert', 'Van Waeyenberg');
 
---
--- Indexen voor geëxporteerde tabellen
---
-
---
--- Indexen voor tabel `employees`
---
 ALTER TABLE `employees`
   ADD PRIMARY KEY (`emp_no`);
 COMMIT;
